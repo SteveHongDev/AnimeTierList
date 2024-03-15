16f0c66adbd417bdb3e1bb372706c5baa17ea2fd
Seongbeom Hong <hong.dev@pm.me>
2024-03-15 18:22:07 +0900

    Init FIrebase

--- AnimeTierList/App/AnimeTierListApp.swift
+++ AnimeTierList/App/AnimeTierListApp.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+    
+}
 
 @main
 struct AnimeTierListApp: App {
     let persistenceController = PersistenceController.shared
     @State private var isShowingSplash = true
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
